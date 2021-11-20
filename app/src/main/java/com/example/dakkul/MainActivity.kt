a04fb1f998724fef7eb22ea70c9596b29e48e887
안나영 <dpswpfskdud@naver.com>
2021-11-21 01:08:47 +0900

    [feat] Project Setting

---
+++ app/src/main/java/com/example/dakkul/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.dakkul
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
+
+    }
+}
