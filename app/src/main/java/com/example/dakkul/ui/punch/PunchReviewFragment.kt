835bf7537f4cc2d3677f228c18b89b3ff41ada71
안나영 <dpswpfskdud@naver.com>
2021-11-21 02:20:33 +0900

    [layout] PunchFragment Layout

--- app/src/main/java/com/example/dakkul/ui/punch/PunchReviewFragment.kt
+++ app/src/main/java/com/example/dakkul/ui/punch/PunchReviewFragment.kt
@@ -6,22 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.dakkul.R
-import com.example.dakkul.databinding.FragmentPunchBinding
 
 
-class PunchFragment : Fragment() {
-
-
-    lateinit var binding:FragmentPunchBinding
+class PunchReviewFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentPunchBinding.inflate(inflater,container,false)
-
-        return binding.root
+        return inflater.inflate(R.layout.fragment_punch_review, container, false)
     }
 
 }
