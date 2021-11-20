8c9c2009292d4a48ba3044c0e775f9fd0ad7dfa0
Jaehun-Jo <chojh922@gmail.com>
2021-11-21 05:08:02 +0900

    [feat] chip 기능 구현

--- app/src/main/java/com/example/dakkul/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/dakkul/ui/home/HomeFragment.kt
@@ -1,22 +1,25 @@
 package com.example.dakkul.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
 import com.example.dakkul.R
 import com.example.dakkul.data.Story
 import com.example.dakkul.databinding.FragmentHomeBinding
 import com.example.dakkul.ui.home.adapter.HomeRVAdapter
+import com.google.android.material.chip.Chip
 
 class HomeFragment : Fragment() {
-    private lateinit var binding : FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
     private lateinit var homeRVAdapter: HomeRVAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         init()
@@ -25,6 +28,34 @@ class HomeFragment : Fragment() {
 
     private fun init() {
         initAdapter()
+
+        Log.d("chip_id", binding.cgHomeTag.checkedChipIds.size.toString())
+
+//        binding.chipHomeAll.setOnCheckedChangeListener { compoundButton, b ->
+//            if (binding.chipHomeAll.isChecked) {
+//                binding.cgHomeTag.clearCheck()
+//                binding.cgHomeTag.check(binding.chipHomeAll.id)
+//            }
+//        }
+
+        binding.cgHomeTag.children.forEach {
+            (it as Chip).setOnCheckedChangeListener { buttonView, isChecked ->
+                if (it.id != binding.chipHomeAll.id) {
+                    if (binding.chipHomeAll.isChecked) {
+                        binding.chipHomeAll.isChecked = false
+                    }
+                    if(binding.cgHomeTag.checkedChipIds.size > 3) {
+                        it.isChecked = false
+                    }
+                }
+                else {
+                    if (binding.chipHomeAll.isChecked) {
+                        binding.cgHomeTag.clearCheck()
+                        binding.cgHomeTag.check(binding.chipHomeAll.id)
+                    }
+                }
+            }
+        }
     }
 
     private fun initAdapter() {
@@ -32,10 +63,10 @@ class HomeFragment : Fragment() {
         homeRVAdapter = HomeRVAdapter()
         homeRVAdapter.itemList.addAll(
             listOf(
-                Story("","","", testList),
-                Story("","","", testList),
-                Story("","","", testList),
-                Story("","","", testList)
+                Story("", "", "", testList),
+                Story("", "", "", testList),
+                Story("", "", "", testList),
+                Story("", "", "", testList)
             )
         )
         binding.rvHomeTargetList.adapter = homeRVAdapter
