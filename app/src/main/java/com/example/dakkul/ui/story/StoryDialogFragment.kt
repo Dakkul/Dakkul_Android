f33fda84df098f9618cbac650c632bfeb934a54c
hyein <hyein2072@gmail.com>
2021-11-21 02:22:16 +0900

    [feat]StoryDialogFragment init setting

---
+++ app/src/main/java/com/example/dakkul/ui/story/StoryDialogFragment.kt
@@ -0,0 +1,35 @@
+package com.example.dakkul.ui.story
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.dakkul.R
+import com.example.dakkul.databinding.FragmentStoryDialogBinding
+
+class StoryDialogFragment : DialogFragment() {
+    private var _binding: FragmentStoryDialogBinding? = null
+    private val binding get() = _binding ?: error("binding이 초기화 되지 않았습니다")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 화면 밖 터치 시 종료 막음
+        isCancelable = true
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentStoryDialogBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
