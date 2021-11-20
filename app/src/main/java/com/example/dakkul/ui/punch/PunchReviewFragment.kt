c8e13d739518a73745a6a0e9c1e4e9d1ec4a8f0d
안나영 <dpswpfskdud@naver.com>
2021-11-21 03:40:38 +0900

    [feat] Navigation Args

--- app/src/main/java/com/example/dakkul/ui/punch/PunchReviewFragment.kt
+++ app/src/main/java/com/example/dakkul/ui/punch/PunchReviewFragment.kt
@@ -1,28 +1,47 @@
 package com.example.dakkul.ui.punch
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import com.example.dakkul.R
 import com.example.dakkul.databinding.FragmentPunchBinding
+import com.example.dakkul.databinding.FragmentPunchReviewBinding
 
 
 class PunchReviewFragment : Fragment() {
 
-    lateinit var binding:FragmentPunchBinding
+    lateinit var binding: FragmentPunchReviewBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentPunchBinding.inflate(inflater,container,false)
+        binding = FragmentPunchReviewBinding.inflate(inflater,container,false)
         return binding.root
     }
 
+    fun setListeners(){
+        binding.etPunchReview.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
 
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
 
 
 }
