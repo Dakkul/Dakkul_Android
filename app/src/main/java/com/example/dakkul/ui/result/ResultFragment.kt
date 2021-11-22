a3156fcdead5ce873d1919c200248f07a1956d01
안나영 <dpswpfskdud@naver.com>
2021-11-22 20:13:42 +0900

    [feat] Resultfragment

--- app/src/main/java/com/example/dakkul/ui/result/ResultFragment.kt
+++ app/src/main/java/com/example/dakkul/ui/result/ResultFragment.kt
@@ -7,21 +7,48 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.dakkul.R
+import com.example.dakkul.data.RetrofitBuilder
 import com.example.dakkul.databinding.FragmentResultBinding
+import com.example.dakkul.ui.punch.PunchFragmentArgs
+import kotlinx.coroutines.*
 
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding ?: error("binding이 초기화 되지 않았습니다")
+
+    private val args by navArgs<ResultFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentResultBinding.inflate(layoutInflater, container, false)
-
+        setView()
         btnClickSet()
         return binding.root
+    }
+
+    private fun setView(){
+        Glide.with(requireContext())
+                .load(args.after)
+                .into(binding.imageView13)
+        /*CoroutineScope(Dispatchers.IO).launch {
+            val response = RetrofitBuilder.dakkulAPI.getResult(1)
+            withContext(Dispatchers.Main){
+                if(response.status==200) {
+                    binding.tv8.text=response.data[3].content
+
+                    //findNavController().navigate(R.id.action_punchReviewFragment_to_resultFragment)
+                }else{
+
+                }
+
+            }
+        }*/
+
     }
 
     override fun onDestroyView() {
@@ -31,7 +58,7 @@ class ResultFragment : Fragment() {
 
     private fun btnClickSet(){
         binding.ivRevengeEnd.setOnClickListener {
-            //findNavController().navigate(R.id.)
+            findNavController().navigate(R.id.action_resultFragment_to_homeFragment)
         }
     }
 }
